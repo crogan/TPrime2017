77bae1911acaf2e2c00550af12cdd8ae1bc3e5f8
Christopher Rogan <crogan@cern.ch>
2017-10-12 11:58:46 -0500

    interval plotting macros

---
+++ macros/PlotSampleBkg.C
@@ -0,0 +1,400 @@
+#include <iostream>
+#include <string>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TBranch.h>
+#include <TGraph.h>
+#include <TMultiGraph.h>
+#include <TAxis.h>
+#include <TCanvas.h>
+#include <TMath.h>
+#include <TLegend.h>
+#include <TLatex.h>
+#include <TObjArray.h>
+#include <TEfficiency.h>
+#include <TH1D.h>
+#include <TChain.h>
+#include <TGraphAsymmErrors.h>
+#include <TStyle.h>
+
+#include "RestFrames/RestFrames.hh"
+#include "include/ReducedBase.hh"
+#include "include/EventCount.hh"
+
+double MTprime(TLorentzVector Top, TLorentzVector Higgs, int type = 0);
+double g_mHiggs;
+double g_mTop;
+
+std::pair<float,float> GetInterval(vector<float>& MT, float sigma);
+
+void PlotSampleBkg(){
+
+  g_mHiggs = 125.;
+  g_mTop   = 175.;
+  
+  vector<int> g_Color;
+  g_Color.push_back(kBlue+2);
+  g_Color.push_back(kRed+2);
+  g_Color.push_back(kGreen+2);
+  g_Color.push_back(kMagenta+2);
+  g_Color.push_back(kCyan+2);
+  g_Color.push_back(kOrange+2);
+  
+  vector<string> g_Files;
+
+  // g_Files.push_back("TbtH_800_LH.root");
+  // g_Files.push_back("TbtH_800_RH.root");
+  // g_Files.push_back("TbtH_900_LH.root");
+  // g_Files.push_back("TbtH_900_RH.root");
+  // g_Files.push_back("TbtH_1000_LH.root");
+  // g_Files.push_back("TbtH_1000_RH.root");
+  // g_Files.push_back("TbtH_1100_LH.root");
+  // g_Files.push_back("TbtH_1100_RH.root");
+  // g_Files.push_back("TbtH_1200_LH.root");
+  // g_Files.push_back("TbtH_1200_RH.root");
+  // g_Files.push_back("TbtH_1300_LH.root");
+  // g_Files.push_back("TbtH_1300_RH.root");
+  // g_Files.push_back("TbtH_1400_LH.root");
+  // g_Files.push_back("TbtH_1400_RH.root");
+  // g_Files.push_back("TbtH_1500_LH.root");
+  // g_Files.push_back("TbtH_1500_RH.root");
+  // g_Files.push_back("TbtH_1700_LH.root");
+  // g_Files.push_back("TbtH_1700_RH.root");
+  // g_Files.push_back("TbtH_1800_LH.root");
+  // g_Files.push_back("TbtH_1800_RH.root");
+
+  g_Files.push_back("TttH_800_LH.root");
+  g_Files.push_back("TttH_800_RH.root");
+  g_Files.push_back("TttH_900_RH.root");
+  g_Files.push_back("TttH_1000_LH.root");
+  g_Files.push_back("TttH_1000_RH.root");
+  g_Files.push_back("TttH_1100_LH.root");
+  g_Files.push_back("TttH_1100_RH.root");
+  g_Files.push_back("TttH_1200_LH.root");
+  g_Files.push_back("TttH_1200_RH.root");
+  g_Files.push_back("TttH_1300_LH.root");
+  g_Files.push_back("TttH_1300_RH.root");
+  g_Files.push_back("TttH_1400_LH.root");
+  g_Files.push_back("TttH_1400_RH.root");
+  g_Files.push_back("TttH_1500_LH.root");
+  g_Files.push_back("TttH_1500_RH.root");
+  g_Files.push_back("TttH_1700_LH.root");
+  g_Files.push_back("TttH_1700_RH.root");
+  g_Files.push_back("TttH_1800_LH.root");
+  g_Files.push_back("TttH_1800_RH.root");
+
+  int Nfile = g_Files.size();
+
+  string g_Path = "/Users/crogan/Dropbox/SAMPLES/Tprime/signal/";
+
+  int Nhist = 4; // different types of MTprime calc
+
+  
+  vector<vector<float> > vMT;
+  for(int i = 0; i < Nhist; i++){
+    vMT.push_back(vector<float>());
+  }
+
+  float mean[Nhist][Nfile];
+  float low[Nhist][3][Nfile];
+  float high[Nhist][3][Nfile];
+
+  double x[Nhist][Nfile];
+  double y[Nhist][3][Nfile];
+  
+  TH1D* hist = (TH1D*) new TH1D("hist", "hist", Nfile, -0.5, double(Nfile)-0.5);
+ 
+  for(int f = 0; f < Nfile; f++){
+    // clear vectors
+    for(int i = 0; i < Nhist; i++){
+      vMT[i].clear();
+      mean[i][f] = 0.;
+    }
+    
+    TChain* chain = new TChain("TPrime");
+    chain->Add((g_Path+g_Files[f]).c_str());
+    ReducedBase* base = new ReducedBase(chain);
+
+    int Nentry = base->fChain->GetEntries();
+    for(int e = 0; e < Nentry; e++){
+      base->GetEntry(e);
+
+      if(base->tau3_top/base->tau2_top > 0.57)
+      	continue;
+      
+      TLorentzVector Top, Higgs;
+      Top.SetPtEtaPhiM( base->pT_top,
+			base->eta_top,
+			base->phi_top,
+			base->mass_top );
+      Higgs.SetPtEtaPhiM( base->pT_higgs,
+			  base->eta_higgs,
+			  base->phi_higgs,
+			  base->mass_higgs );
+      
+      for(int h = 0; h < Nhist; h++){
+	double MT = MTprime(Top, Higgs, h);
+	vMT[h].push_back(MT);
+	mean[h][f] += MT;
+      }
+      
+    }
+    delete base;
+    delete chain;
+   
+    pair<float,float> interval;
+    for(int h = 0; h < Nhist; h++){
+      sort(vMT[h].begin(),vMT[h].end());
+      mean[h][f] /= vMT[h].size();
+
+      x[h][f] = f + ((Nhist+2.)/2. - h-1.5)*float(1./(Nhist+2));
+      for(int s = 0; s < 3; s++){
+	interval = GetInterval(vMT[h], 0.5*(s+1));
+	low[h][s][f] = interval.first;
+	high[h][s][f] = interval.second;
+
+	y[h][s][f] = 0.;
+	
+	//cout << interval.first << " " << interval.second << endl;
+      }
+    }
+    
+  }
+
+  // Now loop over backgrounds...
+  vector<string> g_BkgFiles;
+  g_BkgFiles.push_back("bkg/QCDPt.root");
+  g_BkgFiles.push_back("bkg/TTJets.root");
+  //g_BkgFiles.push_back("bkg/ttHJets.root");
+  g_BkgFiles.push_back("bkg/ttWJets.root");
+  g_BkgFiles.push_back("bkg/ttZJets.root");
+  g_BkgFiles.push_back("bkg/ST_antitop.root");
+  g_BkgFiles.push_back("bkg/ST_top.root");
+  g_BkgFiles.push_back("bkg/tHQ.root");
+  //g_BkgFiles.push_back("bkg/DYJets.root");
+  g_BkgFiles.push_back("bkg/WJets.root");
+  g_BkgFiles.push_back("bkg/WW.root");
+  g_BkgFiles.push_back("bkg/WZ.root");
+  g_BkgFiles.push_back("bkg/ZJets.root");
+  g_BkgFiles.push_back("bkg/ZZ.root");
+
+  int NBkgfile = g_BkgFiles.size();
+
+  string g_BkgPath = "/Users/crogan/Dropbox/SAMPLES/Tprime/";
+
+  for(int b = 0; b < NBkgfile; b++){
+    
+    TChain* chain = new TChain("TPrime");
+    chain->Add((g_BkgPath+g_BkgFiles[b]).c_str());
+    ReducedBase* base = new ReducedBase(chain);
+
+    int Nentry = base->fChain->GetEntries();
+    for(int e = 0; e < Nentry; e++){
+      base->GetEntry(e);
+
+      if(base->tau3_top/base->tau2_top > 0.57)
+      	continue;
+      
+      TLorentzVector Top, Higgs;
+      Top.SetPtEtaPhiM( base->pT_top,
+			base->eta_top,
+			base->phi_top,
+			base->mass_top );
+      Higgs.SetPtEtaPhiM( base->pT_higgs,
+			  base->eta_higgs,
+			  base->phi_higgs,
+			  base->mass_higgs );
+      
+      for(int h = 0; h < Nhist; h++){
+	double MT = MTprime(Top, Higgs, h);
+
+	for(int f = 0; f < Nfile; f++){
+	  for(int s = 0; s < 3; s++){
+	    if(MT >= low[h][s][f] && MT <= high[h][s][f])
+	      y[h][s][f] += base->weight;
+	  }
+	}
+      }
+    }
+    delete base;
+    delete chain;
+  }
+
+  for(int f = 0; f < Nfile; f++){
+    for(int s = 0; s < 3; s++){
+      for(int h = Nhist-1; h >= 0; h--){
+  	y[h][s][f] /= y[0][s][f];
+      }
+    }
+  }
+  
+  
+  TGraph* gr[Nhist][3];
+  
+  for(int h = 0; h < Nhist; h++){
+    for(int s = 0; s < 3; s++){
+      gr[h][s] = (TGraph*) new TGraph(Nfile, x[h], y[h][s]);
+      if(s == 0){
+	gr[h][s]->SetLineWidth(0);
+	gr[h][s]->SetMarkerSize(1);
+	gr[h][s]->SetMarkerStyle(3);
+      }
+      if(s == 1){
+	gr[h][s]->SetLineWidth(0);
+	gr[h][s]->SetMarkerSize(1);
+	gr[h][s]->SetMarkerStyle(3);
+      }
+      if(s == 2){
+	gr[h][s]->SetLineWidth(0);
+	gr[h][s]->SetMarkerSize(1);
+	gr[h][s]->SetMarkerStyle(3);
+      }
+      gr[h][s]->SetMarkerColor(g_Color[h]);
+      gr[h][s]->SetLineColor(g_Color[h]);
+      gr[h][s]->SetFillColor(kWhite);
+    }
+  }
+ 
+  gStyle->SetOptTitle(0);
+  gStyle->SetOptStat(0);
+  gStyle->SetOptFit(11111111);
+  TCanvas* can = (TCanvas*) new TCanvas("can","can",600.,500);
+
+  can->SetLeftMargin(0.13);
+  can->SetRightMargin(0.04);
+  can->SetBottomMargin(0.28);
+  can->SetTopMargin(0.085);
+  can->SetGridx();
+  can->SetGridy();
+  can->Draw();
+  can->cd();
+  for(int s = 0; s < Nfile; s++){
+    // hist->SetBinContent(s+1, 0.);
+    // hist->SetBinError(s+1, 0.);
+    string label = g_Files[s];
+    if(label.find(".root") != string::npos )
+      label.erase(label.find(".root"));
+    hist->GetXaxis()->SetBinLabel(s+1,label.c_str());
+  }
+  hist->LabelsOption("v","X");
+  hist->SetLineColor(kBlack);
+  hist->Draw();
+  hist->GetXaxis()->CenterTitle();
+  hist->GetXaxis()->SetTitleFont(132);
+  hist->GetXaxis()->SetTitleSize(0.06);
+  hist->GetXaxis()->SetTitleOffset(1.06);
+  hist->GetXaxis()->SetLabelFont(132);
+  hist->GetXaxis()->SetLabelSize(0.05);
+  //h_NUM[0]->GetXaxis()->SetTitle("Signal");
+  hist->GetYaxis()->CenterTitle();
+  hist->GetYaxis()->SetTitleFont(132);
+  hist->GetYaxis()->SetTitleSize(0.06);
+  hist->GetYaxis()->SetTitleOffset(1.);
+  hist->GetYaxis()->SetLabelFont(132);
+  hist->GetYaxis()->SetLabelSize(0.05);
+  hist->GetYaxis()->SetTitle("< N_{B} > in 1 #sigma relative to default M_{T'}");
+  hist->GetYaxis()->SetRangeUser(0., 70.);
+  for(int i = 0; i < Nhist; i++)
+    gr[i][2]->Draw("P same");
+ 
+
+  TLatex l;
+  l.SetTextFont(132);
+  l.SetNDC();
+  l.SetTextSize(0.045);
+  l.SetTextFont(132);
+  // l.DrawLatex(0.17,0.855,g_PlotTitle.c_str());
+  l.DrawLatex(0.58,0.943, "Region D Selection, #epsilon_{top} = 0.3");
+  l.SetTextSize(0.04);
+  l.SetTextFont(42);
+  l.DrawLatex(0.13,0.943,"#bf{#it{CMS}} 13 TeV Simulation");
+
+  TLegend* leg = new TLegend(0.1588,0.68,0.3946,0.8947);
+  leg->SetTextFont(132);
+  leg->SetTextSize(0.045);
+  leg->SetFillColor(kWhite);
+  leg->SetLineColor(kWhite);
+  leg->SetShadowColor(kWhite);
+  leg->AddEntry(gr[0][0], "M_{T'} default", "p");
+  leg->AddEntry(gr[1][0], "#tilde{M}_{T'}", "p");
+  leg->AddEntry(gr[2][0], "fixed p_{top/Higgs}", "p");
+  leg->AddEntry(gr[3][0], "fixed E_{top/Higgs}", "p");
+  leg->SetLineColor(kWhite);
+  leg->SetFillColor(kWhite);
+  leg->SetShadowColor(kWhite);
+  leg->Draw("SAME");
+  
+  
+}
+
+double MTprime(TLorentzVector Top, TLorentzVector Higgs, int type){
+  if(type == 0){
+    return (Top+Higgs).M();
+  }
+  if(type == 1){
+    return (Top+Higgs).M() - Top.M() - Higgs.M() + g_mHiggs + g_mTop;
+  }
+  if(type == 2){
+    Top.SetPtEtaPhiM( Top.Pt(), Top.Eta(), Top.Phi(), g_mTop);
+    Higgs.SetPtEtaPhiM( Higgs.Pt(), Higgs.Eta(), Higgs.Phi(), g_mHiggs);
+    return (Top+Higgs).M();
+  }
+  if(type == 3){
+    double Ptop = sqrt(Top.E()*Top.E() - g_mTop*g_mTop);
+    Top.SetPtEtaPhiE( Top.Pt()*Ptop/Top.P(), Top.Eta(), Top.Phi(), Top.E());
+    double Phiggs = sqrt(Higgs.E()*Higgs.E() - g_mHiggs*g_mHiggs);
+    Higgs.SetPtEtaPhiE( Higgs.Pt()*Phiggs/Higgs.P(), Higgs.Eta(), Higgs.Phi(), Higgs.E());
+    return (Top+Higgs).M();
+  }
+  if(type == 4){
+    return ((g_mTop/Top.M())*Top+(g_mHiggs/Higgs.M())*Higgs).M();
+  }
+  if(type == 5){
+    TVector3 boost = (Top+Higgs).BoostVector();
+    Top.Boost(-boost);
+    Higgs.Boost(-boost);
+    return sqrt(Top.P()*Top.P()+g_mTop*g_mTop)+sqrt(Higgs.P()*Higgs.P()+g_mHiggs*g_mHiggs);
+  }
+
+}
+
+std::pair<float,float> GetInterval(vector<float>& MT, float sigma){
+  double P = TMath::Erf(sigma/sqrt(2.));
+  int N = MT.size();
+
+  double Dmin = fabs(MT[N-1]-MT[0]);
+  int imin = 0;
+  for(int i = 0; i < N-1; i++)
+    if(fabs(MT[i+1]-MT[i]) < Dmin){
+      Dmin = fabs(MT[i+1]-MT[i]);
+      imin = i;
+    }
+
+  int jmin = imin+1;
+  int interval = N*P-2;
+  while(interval > 0){
+    if(imin == 0){
+      interval--;
+      jmin++;
+      continue;
+    }
+    if(jmin == N-1){
+      interval--;
+      imin--;
+      continue;
+    }
+    if( fabs(MT[imin]-MT[imin-1]) < fabs(MT[jmin+1]-MT[jmin]) ){
+      interval--;
+      imin--;
+      continue;
+    } else {
+      interval--;
+      jmin++;
+      continue;
+    }
+  }
+
+  pair<float,float> ret(MT[imin],MT[jmin]);
+  return ret;
+}
---
+++ macros/Plot_MTprime.C
@@ -0,0 +1,522 @@
+#include <iostream>
+#include <string>
+
+#include <TFile.h>
+#include <TTree.h>
+#include <TBranch.h>
+#include <TGraph.h>
+#include <TMultiGraph.h>
+#include <TAxis.h>
+#include <TCanvas.h>
+#include <TMath.h>
+#include <TLegend.h>
+#include <TLatex.h>
+#include <TColor.h>
+#include <TColorWheel.h>
+#include <TH1D.h>
+#include <TStyle.h>
+#include <TLorentzVector.h>
+
+#include "include/ReducedBase.hh"
+
+
+using namespace std;
+
+string g_Path;
+vector<string> g_File;
+vector<string> g_Tree;
+vector<int> g_Hist;
+vector<string> g_Title;
+vector<bool> g_Bkg;
+double g_Lumi;
+string g_PlotTitle;
+string g_Xname;
+double g_Xmin;
+double g_Xmax;
+double g_NX;
+
+TColor *icolor[9][2];
+int color_list[4][10];
+int style_list[4][10];
+
+void setstyle(int istyle);
+
+double MTprime(TLorentzVector Top, TLorentzVector Higgs, int type = 0);
+double g_mHiggs;
+double g_mTop;
+
+std::pair<float,float> GetInterval(vector<float>& MT, float sigma);
+
+void Plot_MTprime(){
+  setstyle(0);
+
+  g_mHiggs = 125.;
+  g_mTop   = 175.;
+  
+  vector<int> g_Color;
+  g_Color.push_back(kBlue+2);
+  g_Color.push_back(kRed+2);
+  g_Color.push_back(kGreen+2);
+  g_Color.push_back(kMagenta+2);
+  g_Color.push_back(kCyan+2);
+  g_Color.push_back(kOrange+2);
+  
+  // g_File.push_back("signal/TbtH_1200_LH.root");
+  // g_PlotTitle = "TbtH LH M_{T'} = 1.2 TeV";
+
+  // g_File.push_back("signal/TttH_1200_RH.root");
+  // g_PlotTitle = "TttH RH M_{T'} = 1.2 TeV";
+
+  // g_File.push_back("signal/TbtH_1500_LH.root");
+  // g_PlotTitle = "TbtH LH M_{T'} = 1.5 TeV";
+
+  g_File.push_back("signal/TttH_1500_RH.root");
+  g_PlotTitle = "TttH RH M_{T'} = 1.5 TeV";
+
+  // g_File.push_back("signal/TbtH_1500_LH.root");
+  // g_Hist.push_back(ihist);
+  // g_Title.push_back("TbtH LH M_{T'} = 1.5 TeV");
+  // g_Bkg.push_back(false);
+  // ihist++;
+
+  // g_File.push_back("signal/TbtH_1800_LH.root");
+  // g_Hist.push_back(ihist);
+  // g_Title.push_back("TbtH LH M_{T'} = 1.8 TeV");
+  // g_Bkg.push_back(false);
+  // ihist++;
+
+  // g_File.push_back("signal/TttH_1200_RH.root");
+  // g_Hist.push_back(ihist);
+  // g_Title.push_back("TttH RH");
+  // g_Bkg.push_back(false);
+  // ihist++;
+  
+  int Nsample = g_File.size();
+
+  g_Path = "/Users/crogan/Dropbox/SAMPLES/Tprime/";
+  
+  g_Xname = "M_{T'} alternatives";
+  g_Xmin = 1050.;
+  g_Xmax = 1750.;
+
+  // g_Xmin = 0.;
+  // g_Xmax = 1.;
+  g_NX = 40;
+
+  int Nhist = 4;
+
+  TH1D* hist[Nhist];
+  vector<vector<float> > vMT;
+  for(int i = 0; i < Nhist; i++){
+    vMT.push_back(vector<float>());
+    hist[i] = new TH1D(("h"+to_string(i)).c_str(),
+		       ("h"+to_string(i)).c_str(),
+		       g_NX,g_Xmin,g_Xmax);
+  }
+
+  
+  for(int s = 0; s < Nsample; s++){
+    TChain* chain = new TChain("TPrime");
+    chain->Add((g_Path+g_File[s]).c_str());
+
+    ReducedBase* base = new ReducedBase(chain);
+
+    int Nentry = base->fChain->GetEntries();
+
+    cout << "Sample " << s << " | " << Nsample << endl;
+    for(int e = 0; e < Nentry; e++){
+      base->GetEntry(e);
+      if(e%(max(1,Nentry/10)) == 0)
+	cout << "event " << e << " | " << Nentry << endl;
+
+      if(base->tau3_top/base->tau2_top > 0.6)
+      	continue;
+
+      TLorentzVector Top, Higgs;
+      Top.SetPtEtaPhiM( base->pT_top,
+			base->eta_top,
+			base->phi_top,
+			base->mass_top );
+      Higgs.SetPtEtaPhiM( base->pT_higgs,
+			  base->eta_higgs,
+			  base->phi_higgs,
+			  base->mass_higgs );
+
+      for(int h = 0; h < Nhist; h++){
+	double MT = MTprime(Top, Higgs, h);
+	hist[h]->Fill(MT, base->weight);
+	vMT[h].push_back(MT);
+	//hist[h]->Fill(base->tau3_top/base->tau2_top, base->weight);
+      }
+
+    }
+
+    delete base;
+    delete chain;
+  }
+
+  // Get Intervals
+  vector<pair<float,float> > intervals;
+  for(int i = 0; i < Nhist; i++){
+    sort(vMT[i].begin(),vMT[i].end());
+    intervals.push_back(GetInterval(vMT[i], 0.5));
+    cout << intervals[i].first << " " << intervals[i].second << endl;
+  }
+    
+  double max = -1.;
+  int imax = -1;
+  for(int i = 0; i < Nhist; i++){
+    hist[i]->Scale(1./hist[i]->Integral());
+    if(hist[i]->GetMaximum() > max){
+      max = hist[i]->GetMaximum();
+      imax = i;
+    }
+  }
+  
+  float width = hist[0]->GetBinWidth(1);
+  char *yaxis = new char[100];
+  sprintf(yaxis,"Events / %f", width);
+
+  gStyle->SetOptTitle(0);
+  gStyle->SetOptStat(0);
+  gStyle->SetOptFit(11111111);
+  TCanvas* can = (TCanvas*) new TCanvas("can","can",600.,500);
+
+  can->SetLeftMargin(0.13);
+  can->SetRightMargin(0.04);
+  can->SetBottomMargin(0.15);
+  can->SetTopMargin(0.085);
+  can->SetGridx();
+  can->SetGridy();
+  can->Draw();
+  can->cd();
+  hist[imax]->Draw();
+  hist[imax]->GetXaxis()->CenterTitle();
+  hist[imax]->GetXaxis()->SetTitleFont(132);
+  hist[imax]->GetXaxis()->SetTitleSize(0.06);
+  hist[imax]->GetXaxis()->SetTitleOffset(1.06);
+  hist[imax]->GetXaxis()->SetLabelFont(132);
+  hist[imax]->GetXaxis()->SetLabelSize(0.05);
+  hist[imax]->GetXaxis()->SetTitle(g_Xname.c_str());
+  hist[imax]->GetYaxis()->CenterTitle();
+  hist[imax]->GetYaxis()->SetTitleFont(132);
+  hist[imax]->GetYaxis()->SetTitleSize(0.06);
+  hist[imax]->GetYaxis()->SetTitleOffset(1.);
+  hist[imax]->GetYaxis()->SetLabelFont(132);
+  hist[imax]->GetYaxis()->SetLabelSize(0.05);
+  hist[imax]->GetYaxis()->SetTitle("a. u.");
+  hist[imax]->GetYaxis()->SetRangeUser(0.0001, hist[imax]->GetMaximum()*1.1);
+  //hist[imax]->GetYaxis()->SetTitle(yaxis);
+  //hist[imax]->GetYaxis()->SetTitle("N_{evt} / fb^{-1}");
+  int Ntype[3];
+
+  Ntype[0] = 0;
+  for(int i = 0; i < Nhist; i++){
+    hist[i]->SetLineColor(g_Color[i]);
+    hist[i]->SetLineWidth(3);
+    hist[i]->SetMarkerColor(g_Color[i]);
+    hist[i]->SetMarkerSize(0);
+    hist[i]->SetFillColor(kWhite);
+    Ntype[0]++;
+    hist[i]->Draw("SAME");
+  }
+
+  TLegend* leg = new TLegend(0.1588,0.68,0.3946,0.8947);
+  leg->SetTextFont(132);
+  leg->SetTextSize(0.045);
+  leg->SetFillColor(kWhite);
+  leg->SetLineColor(kWhite);
+  leg->SetShadowColor(kWhite);
+  leg->AddEntry(hist[0], "M_{T'} default", "l");
+  leg->AddEntry(hist[1], "#tilde{M}_{T'}", "l");
+  leg->AddEntry(hist[2], "fixed p_{top/Higgs}", "l");
+  leg->AddEntry(hist[3], "fixed E_{top/Higgs}", "l");
+  leg->SetLineColor(kWhite);
+  leg->SetFillColor(kWhite);
+  leg->SetShadowColor(kWhite);
+  leg->Draw("SAME");
+
+  TLatex l;
+  l.SetTextFont(132);
+  l.SetNDC();
+  l.SetTextSize(0.05);
+  l.SetTextFont(132);
+  // l.DrawLatex(0.17,0.855,g_PlotTitle.c_str());
+  l.DrawLatex(0.6,0.943,g_PlotTitle.c_str());
+  l.SetTextSize(0.04);
+  l.SetTextFont(42);
+  l.DrawLatex(0.15,0.943,"#bf{#it{CMS}} 13 TeV Simulation");	
+
+  // l.SetTextSize(0.045);
+  // l.SetTextFont(132);
+  // string bla = "#scale[0.6]{#int} #it{L dt} = "+to_string(int(g_lumi))+" fb^{-1},  #Delta_{N#scale[0.8]{bkg}} = ";
+  // bla += to_string(int(g_deltaNbkg))+" %";
+  // l.DrawLatex(0.61,0.943,bla.c_str());
+
+
+}
+
+void setstyle(int istyle) {
+	
+  // For the canvas:
+  gStyle->SetCanvasBorderMode(0);
+  gStyle->SetCanvasColor(kWhite);
+  gStyle->SetCanvasDefH(300); //Height of canvas
+  gStyle->SetCanvasDefW(600); //Width of canvas
+  gStyle->SetCanvasDefX(0);   //POsition on screen
+  gStyle->SetCanvasDefY(0);
+	
+  // For the Pad:
+  gStyle->SetPadBorderMode(0);
+  // gStyle->SetPadBorderSize(Width_t size = 1);
+  gStyle->SetPadColor(kWhite);
+  gStyle->SetPadGridX(false);
+  gStyle->SetPadGridY(false);
+  gStyle->SetGridColor(0);
+  gStyle->SetGridStyle(3);
+  gStyle->SetGridWidth(1);
+	
+  // For the frame:
+  gStyle->SetFrameBorderMode(0);
+  gStyle->SetFrameBorderSize(1);
+  gStyle->SetFrameFillColor(0);
+  gStyle->SetFrameFillStyle(0);
+  gStyle->SetFrameLineColor(1);
+  gStyle->SetFrameLineStyle(1);
+  gStyle->SetFrameLineWidth(1);
+	
+  // set the paper & margin sizes
+  gStyle->SetPaperSize(20,26);
+  gStyle->SetPadTopMargin(0.065);
+  gStyle->SetPadRightMargin(0.065);
+  gStyle->SetPadBottomMargin(0.15);
+  gStyle->SetPadLeftMargin(0.17);
+	
+  // use large Times-Roman fonts
+  gStyle->SetTitleFont(132,"xyz");  // set the all 3 axes title font
+  gStyle->SetTitleFont(132," ");    // set the pad title font
+  gStyle->SetTitleSize(0.06,"xyz"); // set the 3 axes title size
+  gStyle->SetTitleSize(0.06," ");   // set the pad title size
+  gStyle->SetLabelFont(132,"xyz");
+  gStyle->SetLabelSize(0.05,"xyz");
+  gStyle->SetLabelColor(1,"xyz");
+  gStyle->SetTextFont(132);
+  gStyle->SetTextSize(0.08);
+  gStyle->SetStatFont(132);
+	
+  // use bold lines and markers
+  gStyle->SetMarkerStyle(8);
+  gStyle->SetHistLineWidth(1.85);
+  gStyle->SetLineStyleString(2,"[12 12]"); // postscript dashes
+	
+  //..Get rid of X error bars
+  gStyle->SetErrorX(0.001);
+	
+  // do not display any of the standard histogram decorations
+  gStyle->SetOptTitle(0);
+  gStyle->SetOptStat(0);
+  gStyle->SetOptFit(11111111);
+	
+  // put tick marks on top and RHS of plots
+  gStyle->SetPadTickX(1);
+  gStyle->SetPadTickY(1);
+	
+  // set a decent palette
+  gStyle->SetPalette(1);
+	
+  gStyle->cd();
+	
+  TColorWheel *w = new TColorWheel();
+	
+  icolor[0][1] = new TColor(1390, 0.90, 0.60, 0.60, ""); //red
+  icolor[0][0] = new TColor(1391, 0.70, 0.25, 0.25, "");
+  icolor[1][1] = new TColor(1392, 0.87, 0.87, 0.91, ""); //blue
+  icolor[1][0] = new TColor(1393, 0.59, 0.58, 0.91, "");
+  icolor[2][1] = new TColor(1394, 0.65, 0.55, 0.85, ""); //violet (gamma)
+  icolor[2][0] = new TColor(1395, 0.49, 0.26, 0.64, "");
+  icolor[3][1] = new TColor(1396, 0.95, 0.95, 0.60, ""); // yellow (alpha)
+  icolor[3][0] = new TColor(1397, 0.95, 0.95, 0.00, "");
+  icolor[4][1] = new TColor(1398, 0.75, 0.92, 0.68, ""); //green (2beta+gamma)
+  icolor[4][0] = new TColor(1399, 0.36, 0.57, 0.30, "");
+  icolor[5][1] = new TColor(1400, 0.97, 0.50, 0.09, ""); // orange
+  icolor[5][0] = new TColor(1401, 0.76, 0.34, 0.09, "");
+  icolor[6][1] = new TColor(1402, 0.97, 0.52, 0.75, ""); // pink
+  icolor[6][0] = new TColor(1403, 0.76, 0.32, 0.51, "");
+  icolor[7][1] = new TColor(1404, 0.49, 0.60, 0.82, ""); // dark blue (kpnn)
+  icolor[7][0] = new TColor(1405, 0.43, 0.48, 0.52, "");
+  icolor[8][1] = new TColor(1406, 0.70, 0.70, 0.70, "");  // black
+  icolor[8][0] = new TColor(1407, 0.40, 0.40, 0.40, "");
+	
+	
+  if(istyle == 0){
+		
+    //SM MC
+    color_list[3][0] = kCyan+3;
+		
+    //DATA
+    color_list[0][0] = 1;
+    color_list[0][1] = 2;
+    color_list[0][2] = 4;
+    style_list[0][0] = 20;
+    style_list[0][1] = 23;
+		
+    //BKG MC
+    color_list[1][0] = 0;
+    color_list[1][3] = kGreen-9; //Light green
+    color_list[1][5] = kOrange-2; //dark blue
+    color_list[1][4] = kGreen+3; //yellow
+    color_list[1][1] = kBlue-10; //light blue
+    color_list[1][2] = kBlue+4; //dark green
+    style_list[1][0] = 1001;
+    style_list[1][1] = 1001;
+    style_list[1][2] = 3002;
+    style_list[1][3] = 1001;
+    style_list[1][4] = 1001;
+    style_list[1][5] = 1001;
+		
+    //SIG MC
+    color_list[2][0] = 1;
+    color_list[2][1] = 1;
+    color_list[2][2] = 1;
+    style_list[2][0] = 2;
+    style_list[2][1] = 5;
+  }
+  if(istyle == 1){
+		
+    //SM MC
+    color_list[3][0] = kSpring+4;
+		
+    //DATA
+    color_list[0][0] = 1;
+    color_list[0][1] = 2;
+    color_list[0][2] = 4;
+    style_list[0][0] = 20;
+    style_list[0][1] = 23;
+		
+    //BKG MC
+    color_list[1][0] = 0;
+    color_list[1][1] = kGreen-9; //Light green
+    color_list[1][2] = kGreen+3; //dark blue
+    color_list[1][3] = kYellow-7; //yellow
+    color_list[1][4] = kBlue-10; //light blue
+    color_list[1][5] = kBlue+4; //dark blue
+    style_list[1][0] = 1001;
+    style_list[1][1] = 1001;
+    style_list[1][2] = 3002;
+    style_list[1][3] = 1001;
+    style_list[1][4] = 1001;
+    style_list[1][5] = 1001;
+		
+    //SIG MC
+    color_list[2][0] = 1;
+    color_list[2][1] = 1;
+    color_list[2][2] = 1;
+    style_list[2][0] = 2;
+    style_list[2][1] = 5;
+  }
+  if(istyle == 2){
+		
+    //SM MC
+    color_list[3][0] = kMagenta+2;
+		
+    //DATA
+    color_list[0][0] = 1;
+    color_list[0][1] = 2;
+    color_list[0][2] = 4;
+    style_list[0][0] = 20;
+    style_list[0][1] = 23;
+		
+    //BKG MC
+    color_list[1][0] = 0;
+    color_list[1][3] = kRed-9; //Light green
+    color_list[1][5] = kRed+3; //dark blue
+    color_list[1][4] = kYellow-7; //yellow
+    color_list[1][1] = kMagenta-10; //light blue
+    color_list[1][2] = kMagenta+4; //dark green
+    style_list[1][0] = 1001;
+    style_list[1][1] = 1001;
+    style_list[1][2] = 3002;
+    style_list[1][3] = 1001;
+    style_list[1][4] = 1001;
+    style_list[1][5] = 1001;
+		
+    //SIG MC
+    color_list[2][0] = 1;
+    color_list[2][1] = 1;
+    color_list[2][2] = 1;
+    style_list[2][0] = 2;
+    style_list[2][1] = 5;
+  }
+	
+}
+
+double MTprime(TLorentzVector Top, TLorentzVector Higgs, int type){
+  if(type == 0){
+    return (Top+Higgs).M();
+  }
+  if(type == 1){
+    return (Top+Higgs).M() - Top.M() - Higgs.M() + g_mHiggs + g_mTop;
+  }
+  if(type == 2){
+    Top.SetPtEtaPhiM( Top.Pt(), Top.Eta(), Top.Phi(), g_mTop);
+    Higgs.SetPtEtaPhiM( Higgs.Pt(), Higgs.Eta(), Higgs.Phi(), g_mHiggs);
+    return (Top+Higgs).M();
+  }
+  if(type == 3){
+    double Ptop = sqrt(Top.E()*Top.E() - g_mTop*g_mTop);
+    Top.SetPtEtaPhiE( Top.Pt()*Ptop/Top.P(), Top.Eta(), Top.Phi(), Top.E());
+    double Phiggs = sqrt(Higgs.E()*Higgs.E() - g_mHiggs*g_mHiggs);
+    Higgs.SetPtEtaPhiE( Higgs.Pt()*Phiggs/Higgs.P(), Higgs.Eta(), Higgs.Phi(), Higgs.E());
+    return (Top+Higgs).M();
+  }
+  if(type == 4){
+    return ((g_mTop/Top.M())*Top+(g_mHiggs/Higgs.M())*Higgs).M();
+  }
+  if(type == 5){
+    TVector3 boost = (Top+Higgs).BoostVector();
+    Top.Boost(-boost);
+    Higgs.Boost(-boost);
+    return sqrt(Top.P()*Top.P()+g_mTop*g_mTop)+sqrt(Higgs.P()*Higgs.P()+g_mHiggs*g_mHiggs);
+  }
+
+}
+
+std::pair<float,float> GetInterval(vector<float>& MT, float sigma){
+  double P = TMath::Erf(sigma/sqrt(2.));
+  int N = MT.size();
+
+  double Dmin = fabs(MT[N-1]-MT[0]);
+  int imin = 0;
+  for(int i = 0; i < N-1; i++)
+    if(fabs(MT[i+1]-MT[i]) < Dmin){
+      Dmin = fabs(MT[i+1]-MT[i]);
+      imin = i;
+    }
+
+  int jmin = imin+1;
+  int interval = N*P-2;
+  while(interval > 0){
+    if(imin == 0){
+      interval--;
+      jmin++;
+      continue;
+    }
+    if(jmin == N-1){
+      interval--;
+      imin--;
+      continue;
+    }
+    if( fabs(MT[imin]-MT[imin-1]) < fabs(MT[jmin+1]-MT[jmin]) ){
+      interval--;
+      imin--;
+      continue;
+    } else {
+      interval--;
+      jmin++;
+      continue;
+    }
+  }
+
+  pair<float,float> ret(MT[imin],MT[jmin]);
+  return ret;
+}
